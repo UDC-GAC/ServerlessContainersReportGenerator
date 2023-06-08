3d9e902505cf312df5381399385753e5abffabe5
JonatanEnes <jonatan.enes@udc.es>
2023-06-09 00:49:46 +0200

    Changed config so that test data can be used publicly

--- set_pythonpath.sh
+++ set_pythonpath.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export BDWATCHDOG_PATH=$HOME/development/BDWatchdog
-export REPORT_GENERATOR_PATH=$HOME/development/ServerlessContainersReportGenerator
+export BDWATCHDOG_PATH=$HOME/Desktop/development/BDWatchdog
+export REPORT_GENERATOR_PATH=$HOME/Desktop/development/ServerlessContainersReportGenerator
 export PYTHONPATH=$BDWATCHDOG_PATH:$REPORT_GENERATOR_PATH
