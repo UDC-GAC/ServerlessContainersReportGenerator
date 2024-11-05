9585334bce4fd23cb273ec8c1b43341c9bbe1e76
JonatanEnes <jonatan.enes@udc.es>
2024-11-05 22:24:09 +0100

    New 2 scripts to generate just the used plots

---
+++ gen_used_transcode_plots.sh
@@ -0,0 +1,5 @@
+bash scripts/generate_report.sh "transcode_basic_2" conf/experiments/blockchain/transcode_basic.ini
+bash scripts/generate_report.sh "transcode_greedy_3" conf/experiments/blockchain/transcode_greedy.ini
+bash scripts/generate_report.sh "transcode_4fold_2" conf/experiments/blockchain/transcode_4fold.ini
+
+
