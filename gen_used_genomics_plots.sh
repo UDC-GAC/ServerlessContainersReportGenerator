04491df7dc7474802817cf02e585eac6c8a7922a
JonatanEnes <jonatan.enes@udc.es>
2024-11-26 19:03:19 +0100

    Fix

--- gen_used_genomics_plots.sh
+++ gen_used_genomics_plots.sh
@@ -1,5 +1,5 @@
 bash scripts/generate_report.sh "genomics_greedy_3" conf/experiments/blockchain/genomics_greedy.ini
 bash scripts/generate_report.sh "genomics_conservative_4" conf/experiments/blockchain/genomics_conservative.ini
-bash scripts/generate_report.sh "genomics_4fold_4" conf/experiments/blockchain/genomics_4fold.ini
+bash scripts/generate_report.sh "genomics_4fold_3" conf/experiments/blockchain/genomics_4fold.ini
 
 
