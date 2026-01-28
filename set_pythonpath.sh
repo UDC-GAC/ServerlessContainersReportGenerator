cf5dcbbe7d90b80a56e25715ad5d9cf5e9a2f3f9
JonatanEnes <jonatan.enes@udc.es>
2026-01-28 12:30:39 +0100

    Cambios acorde a revision de roberto

--- set_pythonpath.sh
+++ set_pythonpath.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export BDWATCHDOG_PATH=$HOME/Desktop/BDWatchdog
-export REPORT_GENERATOR_PATH=$HOME/Desktop/ServerlessContainersReportGenerator
+export BDWATCHDOG_PATH=$HOME/Desktop/BAY/investigacion/BDWatchdog
+export REPORT_GENERATOR_PATH=$HOME/Desktop/BAY/investigacion/ServerlessContainersReportGenerator
 export PYTHONPATH=$BDWATCHDOG_PATH:$REPORT_GENERATOR_PATH
